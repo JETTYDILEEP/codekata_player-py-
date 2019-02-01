7dd1285cd9d7990f03da012b618f465648545b0e
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-01 10:57:58 +0530

    Update pg48.py

--- pg48.py
+++ pg48.py
@@ -1,9 +1,9 @@
 def fun1(n):
-  for i in range(1,n+1):
+  for i in range(1,n):
     fun2(i,n)
 
 def fun2(i,n):
-  if(i!=n):
+  if(i!=n-1):
     if(n%i==0 and i%2!=0):
       print(i,end=' ')
   else:
