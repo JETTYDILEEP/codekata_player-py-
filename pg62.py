0e7874367ae5c027c5e7d2ee3eff05fddde7228d
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-02 20:38:06 +0530

    Update pg62.py

--- pg62.py
+++ pg62.py
@@ -1,7 +1,7 @@
 n=int(input())
 c=0
-for i in range(1,n+1):
-    c=n/i
-    if(c%2!=0):
-        print(i)
-        break
+for i in range(1,n+2):
+  c=n/i
+  if(c%2!=0 and n%i==0):
+      print(i)
+      break
