b8841def0d1621bb55df8ba53ce2227bc9a3f01c
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-13 21:11:16 +0530

    Update pg68.py

--- pg68.py
+++ pg68.py
@@ -1,8 +1,10 @@
 n=int(input())
 l=[int(x) for x in input().split()]
-t=l.count(l[0])
+c=1
 if(n==len(l)):
   for i in range(n):
-    if(l.count(i)>t):
-      t=l.count(i)
-print(t)
+	  for j in range(i+1,n):
+		  if (l.count(i)>1 and l[i]==l[i+1]):
+			  c=c+1
+	  
+print(c)
