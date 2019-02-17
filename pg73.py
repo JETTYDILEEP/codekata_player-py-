e52c66a62e99db9d02a67bc105d0b74b93cbf418
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-17 15:37:07 +0530

    Create pg73.py

---
+++ pg73.py
@@ -0,0 +1,16 @@
+class Bitonic:
+  def ope(N,l,K):
+    for i in range(0,N-1):
+      if(l[i]==K): 
+        print(i+1)
+        break
+      else:
+        continue 
+    
+
+  
+
+N,K=map(int,(input().split()))
+l=[int(x) for x in input().split()]
+if(N==len(l)):
+  Bitonic.ope(N,l,K)
