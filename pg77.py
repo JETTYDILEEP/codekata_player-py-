e9484a7f93c3e87ba9484e2c171479c3c0c14068
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-19 09:54:12 +0530

    Create pg77.py

---
+++ pg77.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=[int(x) for x in input().split()]
+c=1
+if(n==len(l)):
+  for i in range(0,n-1):
+    if(l[i]+1==l[i+1]):
+      c=c+1
+
+
+if(c>1):
+  print(c)
+else:
+  print(c-1)
