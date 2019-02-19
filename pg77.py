a9165fa8716ef23484ea80af5e3ab23a2abe8eef
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-19 10:09:13 +0530

    Update pg77.py

--- pg77.py
+++ pg77.py
@@ -1,15 +1,17 @@
-n=int(input())
-l=[int(x) for x in input().split()]
-c=1
-if(n==len(l)):
-  for i in range(0,n-1):
-    if(l[i]+1==l[i+1]):
-      c=c+1
-    else:
-      break
+class incarr:
+  def fun(n,l):
+    c=1
+    l1=[]
+    if(n==len(l)):
+      for i in range(0,n-1):
+        if(l[i]+1==l[i+1]):
+          c=c+1
+        else:
+          l1.append(c)
+          c=1
+      l1.append(c)
+    print(max(l1))
 
-
-if(c>1):
-  print(c)
-else:
-  print(c-1)
+N=int(input())
+L=[int(x) for x in input().split()]
+incarr.fun(N,L)
