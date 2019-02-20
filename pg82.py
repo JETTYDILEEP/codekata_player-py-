9cf641a47949211b4b190ed2b01cb5456ee0b49c
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-20 09:56:23 +0530

    Update pg82.py

--- pg82.py
+++ pg82.py
@@ -1,13 +1,7 @@
 n=int(input())
-l=[int(x) for x in input().split()]
-t=0
-if(n==len(l)):
-  for i in range(0,n):
-    for j in range(0,n):
-      if((l[j]&l[i])>0 and i!=j ): 
-        t.append(l[i]&l[j])
-      elif(len(l)==1):
-        t=l[0]
-       
-
-print(t)       
+a=[int(x) for x in input().split()]
+if(n==len(a)):
+  s=a[0] 
+  for i in range(1,n):
+    s=s&a[i]
+  print(s)
