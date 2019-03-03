26d7f1a856f63b69cdb06956f426552940e62e44
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-03-03 11:20:02 +0530

    Create pg103.py

---
+++ pg103.py
@@ -0,0 +1,15 @@
+import random
+class pg103:
+    def func(n,l):
+      c=0
+      random.shuffle(l)
+      print(l)
+      if(n==len(l)):
+        for i in range(0,n-1):
+          c=c+max(l[i],l[i+1])
+
+      return c  
+
+n=int(input())
+l=[int(x) for x in input().split()]
+print(pg103.func(n,l))
