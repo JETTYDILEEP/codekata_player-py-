5affaebc6ce57913732505d12e2def6e7ab0c445
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-03-03 10:44:50 +0530

    Create pg102.py

---
+++ pg102.py
@@ -0,0 +1,12 @@
+class pg102:
+  def func(n,l):
+    c=0
+    l.sort()
+    if(n==len(l)):
+      for i in range(0,n-1):
+        c=c+max(l[i],l[i+1])
+    return c    
+
+n=int(input())
+l=[int(x) for x in input().split()]
+print(pg102.func(n,l))
