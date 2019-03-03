940d497b7154bca0d0b9e21800cdfc865c6b9c1a
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-03-03 11:39:21 +0530

    Create pg105.py

---
+++ pg105.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=[int(x) for x in input().split()]
+d={}
+for i in range(n):
+	d[i]=i
+for i in range(n):
+  p=i
+  for j in range(i+1,n):
+    if l[p]>l[j]:
+		    p=j
+  if p!=i:
+  	t=l[i]
+  	l[i]=l[p]
+  	l[p]=t
+  print(d[i]+1,end=" ")
