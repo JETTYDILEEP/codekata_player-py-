0342762b88dfc3f0930d624a89b4c7fd4f302fe0
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-03-04 17:27:02 +0530

    Create pg115.py

---
+++ pg115.py
@@ -0,0 +1,17 @@
+class pg115:
+  def func(s,m):
+    s=list(s)
+    m=list(m)
+    if(len(s)>1):
+      s[len(s)-1]=m[0]
+      for i in range(1,len(m)):
+        s.append(m[i])
+
+      print(*s,sep='')
+    else:
+      for i in range(0,len(m)):
+        s.append(m[i])
+      print(*s,sep='')
+
+s,m=map(str,input().split())
+pg115.func(s,m)
