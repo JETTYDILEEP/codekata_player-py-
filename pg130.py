20811e9f8f59d9982543b54368e6ab567da5a00a
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-03-10 14:25:26 +0530

    Update pg130.py

--- pg130.py
+++ pg130.py
@@ -4,7 +4,11 @@ class pg130:
     if(n==len(l)):
       for i in l:
         c=i+c       
-        g.append(c)
+        if(c%2==0):
+          g.append(c)
+        else:
+          g.append(i)
+        
       print(*g,sep=' ')
 
 n=int(input())
