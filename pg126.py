ed680402cde19a3e4a361214eb4f1447bc7e345e
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-03-10 13:45:24 +0530

    Create pg126.py

---
+++ pg126.py
@@ -0,0 +1,14 @@
+class pg126:
+  def func(n,m,l):
+    g=[]
+    if(len(l)==n):
+      for i in l:
+        c=l.count(i)
+        if(c<m):
+          g.append(i)
+
+    print(*g,sep=' ')
+
+n,m=map(int,input().split())
+l=[int(x) for x in input().split()]
+pg126.func(n,m,l)
