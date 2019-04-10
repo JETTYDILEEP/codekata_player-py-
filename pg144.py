6914b366fa2b1a57cd8d65b63b324206671f1fdb
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-10 20:24:49 +0530

    Update pg144.py

--- pg144.py
+++ pg144.py
@@ -1,9 +1,11 @@
 n=int(input())
 l=[int(x) for x in input().split()]
 if(len(l)==n):
+  t=[]
   for i in range(n):
     if((l[i]%l[i-1])==0):
-      print(l[i],end=' ')    
+      t.append(l[i])    
     else:
       continue
+  print(*t,sep=' ')
   
