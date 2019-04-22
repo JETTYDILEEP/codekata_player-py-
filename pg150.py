fbcea3eae09deefe4c5463cbadad09aedac8a014
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-22 17:30:23 +0530

    Update pg150.py

--- pg150.py
+++ pg150.py
@@ -1,8 +1,8 @@
-def fact(n):
+def fac(n):
 	f=1
 	for i in range(1,n+1):
 		f=f*i
 	return f
-a,b=map(int,input().split())
-x=fact(a)//fact(b)
+a,b,c=map(int,input().split())
+x=(fact(a)//fact(b))//fact(c)
 print(x)
