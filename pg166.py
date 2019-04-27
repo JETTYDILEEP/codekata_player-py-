e21cfd1abbd2ff042d709629995bfd75fa502ed0
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-27 17:16:50 +0530

    Update pg166.py

--- pg166.py
+++ pg166.py
@@ -1,14 +1,9 @@
 class pg166:
-  def gcd(a,b):
-    if(b==0):
-      return a
-    else:
-      return pg166.gcd(b,a%b)
-  
+  def gcd(a):
+    b=1
+    for i in range(1,a+1):
+      b=b*i
+    return b
 m,n=map(int,input().split())
-a,b=1,1
-for i in range(1,m+1):
-  a=a*i
-for i in range(1,n+1):
-  b=b*i
-print(pg166.gcd(a,b))
+a=min(m,n)
+print(pg166.gcd(a))
