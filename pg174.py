270292306aeb8b072d7ae4e4c1740e6c7752db55
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-28 08:07:18 +0530

    Update pg174.py

--- pg174.py
+++ pg174.py
@@ -6,25 +6,28 @@ if (m.isalpha() or " " in m) and (n.isalpha() or " " in n):
     m=list(m.split(" "))
     n=list(n.split(" "))
     for i in m:
+        c=c+1
         if m.count(i) > n.count(i) and i not in l:
-            c=c+1
+            
             t.append(c)
             l.append(i)
     for i in n:
+        c=c+1
         if n.count(i)>m.count(i) and i not in l:
-            c=c+1
             t.append(c)
             l.append(i)
 
 else:
     for i in m:
+        c=c+1
         if m.count(i)>n.count(i) and i not in l:
-            c=c+1
+            
             t.append(c)
             l.append(i)
     for i in n:
+        c=c+1
         if n.count(i)>m.count(i) and i not in l:
-            c=c+1
+            
             t.append(c)
             l.append(i)
 for i in range(len(t)):
