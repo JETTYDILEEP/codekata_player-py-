d9835f415da95cb479dd9aa8b5fd27ff599d2a9e
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-28 11:48:02 +0530

    Create pg176.py

---
+++ pg176.py
@@ -0,0 +1,14 @@
+class pg176:
+    def func(m,n):
+        m=list(m)
+        n=list(n)
+        c=0
+        for i in range(len(n)):
+            if(n[i] not in m):
+                c=c+1
+        if(c>1):
+            print('false')
+        else:
+            print('true')
+m,n=map(str,input().split())
+pg176.func(m,n)
