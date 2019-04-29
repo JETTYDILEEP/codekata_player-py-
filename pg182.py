d24b69c2bbc4496902b110d1350da11ccd859588
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-29 11:21:13 +0530

    Update pg182.py

--- pg182.py
+++ pg182.py
@@ -9,7 +9,7 @@ class pg182:
                 v.append(l[i+1])
         r.append(c)
         r.append(v)
-        print(r.count(min(r)))
+        print(r.count(min(r))+1)
 n=int(input())
 l=[int(x) for x in input().split()]
 pg182.func(n,l)
