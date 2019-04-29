4dd6557fc1c37905d7354fc939c1d0b8fddba48e
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-04-29 11:19:35 +0530

    Create pg182.py

---
+++ pg182.py
@@ -0,0 +1,15 @@
+class pg182:
+    def func(n,l):
+        c,v,r=[],[],[]
+        for i in range(1,n):
+            if(l[i-1]<l[i]):
+                c.append(l[i])
+                
+            elif(l[i-1]==l[i]):
+                v.append(l[i+1])
+        r.append(c)
+        r.append(v)
+        print(r.count(min(r)))
+n=int(input())
+l=[int(x) for x in input().split()]
+pg182.func(n,l)
