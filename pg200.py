2aae4a4fde6bee4793317eb74e200bd7474d5c23
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-05-07 12:31:36 +0530

    Update pg200.py

--- pg200.py
+++ pg200.py
@@ -2,7 +2,7 @@ class pg200:
     def func(n):
         l=[]
         for i in n:
-            if(n.count(i)>1 and i not in l):
+            if(n.count(i)>=1 and i not in l):
                 l.append(i)    
         print(*l,sep='')
 n=str(input())
