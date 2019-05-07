5789ff101867d8ac074443d00391e9f43cb51c49
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-05-07 12:29:19 +0530

    Create pg200.py

---
+++ pg200.py
@@ -0,0 +1,9 @@
+class pg200:
+    def func(n):
+        l=[]
+        for i in n:
+            if(n.count(i)>1 and i not in l):
+                l.append(i)    
+        print(*l,sep='')
+n=str(input())
+pg200.func(n)
