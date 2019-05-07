018fd21a3c910ea60b6551f71b34e3e1e8a3cc74
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-05-07 12:06:58 +0530

    Update pg196.py

--- pg196.py
+++ pg196.py
@@ -1,11 +1,10 @@
 class pg196:
     def func(n,l):
-        c=0
-        for i in range(0,n-1):
-            if(l.count(l[i])<l.count(l[i+1])):
-                c=l[i]
+        for i in range(0,n):
+            if(l.count(l[i])==1):
+                print(l[i])
             
-        print(c)
+        
 n=int(input())
 l=[int(x) for x in input().split()]
 pg196.func(n,l)
