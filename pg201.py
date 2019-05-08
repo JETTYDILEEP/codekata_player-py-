9e06485398fc4f6ac987dba66c5598f5e58d2a77
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-05-08 11:14:15 +0530

    Create pg201.py

---
+++ pg201.py
@@ -0,0 +1,15 @@
+def catalan(n): 
+   
+    if n <= 1 : 
+        return 1 
+  
+    # Catalan(n) is the sum of catalan(i)*catalan(n-i-1) 
+    res = 0 
+    for i in range(n): 
+        res += catalan(i) * catalan(n-i-1) 
+  
+    return res 
+  
+m=int(input()) 
+for i in range(0,m+1): 
+    print(catalan(i),end=' ') 
